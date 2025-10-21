db5d05bf5a272c0a5bb4084d680af4f77bf60c2c
Sichao25 <sichao_y@outlook.com>
2025-10-21 19:30:32 -0400

    add omega_h

--- test_classPtnGather.cpp
+++ test_classPtnGather.cpp
@@ -47,10 +47,11 @@ void test3(const int rank, const EntToRank& expectedE2R) {
 }
 
 int main(int argc, char** argv) {
+  auto lib = Omega_h::Library(&argc, &argv);
+  const auto world = lib.world();
   int rank, nproc;
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+  const int rank = world->rank();
+  const int nproc = world->size();
   std::cout << "Rank " << rank << " / " << nproc << std::endl;
   REDEV_ALWAYS_ASSERT(nproc == 2);
 
